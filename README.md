bd8482be3884337e81b3705b890851f70b45d88e
Canaan Merchant <canaan.merchant@vexterra.com>
2020-01-13 11:28:28 -0500

    package-lock fix

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spotifollowers
 Displaying live Spotify follower counts. 
+
+To-Do: 
+
+[] - Create components for front-end. Components include a landing page, a live counter, and the artist's profile image with QR code.
+[] - Create a back-end component that lets artist's log in to display their live counter. 
+[] - Create a way for artists to write new messages on their splash page. 
+[] - use spotify api to fetch an artist's follower count. 
+[] - use spotify api to fetch artist's image/qr code. 
+[] - use spotify api to authenticate artsists when they log in.  
--- src/components/counter.component.jsx
+++ src/components/counter.component.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Counter() {
-    
+class Counter extends Component() {
+    componentDidMount () {
+        fetch('spotify artist api')
+        .then(response => response.json())
+        .then(artists = > console.log(artists)); 
+    }
     const counter = 1
-
-    return (
-        <div>
-<h1>Follower Count: {counter}</h1>  
-        </div>
-    );
+render(){
+        return (
+            <div>
+                <h1>Follower Count: {counter}</h1>  
+            </div>
+        );
+    }
 }
 
 export default Counter;
